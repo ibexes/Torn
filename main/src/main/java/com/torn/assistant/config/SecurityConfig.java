d61819f9fe3ccb568fb8693a3a627a05d5ebc0cc
ibexes <jiaman@live.co.uk>
2020-12-30 12:04:20 +0000

    lockdown admin commands

--- main/src/main/java/com/torn/assistant/config/SecurityConfig.java
+++ main/src/main/java/com/torn/assistant/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.torn.assistant.config;
 
 import com.torn.assistant.service.AuthenticationService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final AuthenticationService authenticationService;
     private final UserDetailsService userDetailsService;
--- main/src/main/java/com/torn/assistant/controller/FactionGymController.java
+++ main/src/main/java/com/torn/assistant/controller/FactionGymController.java
@@ -8,6 +8,8 @@ import com.torn.assistant.service.FactionStatsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +27,11 @@ public class FactionGymController {
         this.factionStatsService = factionStatsService;
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/api/faction/members/update")
-    public void updateMembers() throws JsonProcessingException, TornApiAccessException {
+    public ResponseEntity<String> updateMembers() throws JsonProcessingException, TornApiAccessException {
         factionStatsService.updateMembers();
+        return ResponseEntity.ok("OK");
     }
 
     @GetMapping("/api/faction/contributions/intervals")
@@ -51,6 +55,7 @@ public class FactionGymController {
         return factionStatsService.getUserContributionDetailedForUser(principal.getName(), userId, start, end);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/api/faction/contributions/poll")
     public void poll() throws JsonProcessingException, TornApiAccessException {
         logger.info("Manual poll invoked");
